da035997742df056f86ff7cbb94ff8e1e79a655b
Laurent <laurentahsue888@gmail.com>
2022-02-11 16:12:09 +0800

    Submission

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -25,6 +25,24 @@ L.tileLayer(
   }
 ).addTo(myMap);
 
+//Creating function to classify earthquake magnitude in different colors
+
+function EarthquakeColor(magnitude) {
+  if (magnitude >= 5) {
+    return "Red";
+  } else if (magnitude >= 4 && magnitude < 4.99) {
+    return "DarkOrange";
+  } else if (magnitude >= 3 && magnitude < 3.99) {
+    return "Orange";
+  } else if (magnitude >= 2 && magnitude < 2.99) {
+    return "Yellow";
+  } else if (magnitude >= 1 && magnitude < 1.99) {
+    return "GreenYellow";
+  } else if (magnitude < 1) {
+    return "Lime";
+  }
+}
+
 // Perform a GET request to the query URL
 d3.json(queryUrl).then((earthquake) => {
   earthquake.features.forEach((feature) => {
@@ -32,12 +50,51 @@ d3.json(queryUrl).then((earthquake) => {
       [feature.geometry.coordinates[1], feature.geometry.coordinates[0]],
       {
         fillOpacity: 0.75,
-        color: "white",
-        fillColor: "purple",
+        color: "black",
+        fillColor: EarthquakeColor(feature.properties.mag),
         // Setting our circle's radius equal to the output of our markerSize function
         // This will make our marker's size proportionate to its magnitude
         radius: feature.properties.mag * 50000,
       }
-    ).addTo(myMap);
+    )
+      .bindPopup(
+        "<h3>" +
+          feature.properties.place +
+          "</h3> <hr> Coordinates : " +
+          [feature.geometry.coordinates[1], feature.geometry.coordinates[0]] +
+          "<br> Magnitude : " +
+          feature.properties.mag
+      )
+      .addTo(myMap);
   });
+
+  // Create a legend that will provide context for your map data.
+
+  var legend = L.control({ position: "bottomright" });
+
+  legend.onAdd = function () {
+    var div = L.DomUtil.create("div", "info legend");
+
+    var grades = [0, 1, 2, 3, 4, 5];
+    var colors = [
+      "Lime",
+      "GreenYellow",
+      "Yellow",
+      "Orange",
+      "DarkOrange",
+      "Red",
+    ];
+
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        "<i style='background: " +
+        colors[i] +
+        "'></i> " +
+        grades[i] +
+        (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+  legend.addTo(myMap);
 });
